7f7f639665f9901519be2d978a8bb381e265d433
Julien BERTRAND <j.bertrand@student.isartdigital.com>
2022-03-14 17:04:28 +0100

    merge

---
+++ Weapon.cs
@@ -0,0 +1,31 @@
+﻿using ImGuiNET;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoStorm
+{
+    class Weapon
+    {
+        float timer = 0.0f;
+        public void Update(GameInputs inputs, GameData data )
+        {
+            if(timer > 0)
+             timer -= inputs.Deltatime;
+
+
+            if (inputs.Shoot && timer <= 0.0f)
+            {
+                Bullet b = new Bullet();
+                b.Position = data.Player.Position;
+                b.SetRotation(data.Player.Rotation);
+                data.AddBullet(b);
+
+                timer += 0.07f;
+            }
+        }
+    }
+}
+    
